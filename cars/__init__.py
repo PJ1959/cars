ee078dd0113db5233d652c48ed47d68747434e9b
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-18 01:43:13 -0300

    Added flask-login LoginManager

--- cars/__init__.py
+++ cars/__init__.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
-app.config["MONGODB_SETTINGS"] = {'DB': "cars"}
-app.config["SECRET_KEY"] = "super_secret_key"
+app.config['MONGODB_SETTINGS'] = {'DB': 'cars'}
+app.config['SECRET_KEY'] = 'super_secret_key'
 
 db = MongoEngine(app)
+
+lm = LoginManager(app)
+lm.init_app(app)
+lm.login_view = 'login'
 
 from . import controllers
 
