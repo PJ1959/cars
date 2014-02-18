9fc34b41eb1cbe1a43a9cdf950be20f7d962b8ed
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-18 01:49:48 -0300

    Added forms and its tests

---
+++ cars/test_forms.py
@@ -0,0 +1,35 @@
+from .forms import RegistrationForm, LoginForm, SearchForm, CarForm
+
+user_data = {'name': 'Jhon Doe', 'email': 'jhon@doe.com',
+             'password': 'jJdD', 'confirm': 'jJdD'}
+
+car_data = {'year': '1938', 'manufacturer': 'Volkswagen',
+            'model': 'Beetle'}
+
+
+def test_registration_form():
+    form = RegistrationForm()
+    form.process(user_data)
+    form.validate()
+    assert form.errors == {}
+
+
+def test_login_form():
+    form = LoginForm()
+    form.process({'username': 'jhon@doe.com', 'password': 'jJdD'})
+    form.validate()
+    assert form.errors == {}
+
+
+def test_search_form():
+    form = SearchForm()
+    form.process({'search': 'Beetle'})
+    form.validate()
+    assert form.errors == {}
+
+
+def test_car_form():
+    form = CarForm()
+    form.process(car_data)
+    form.validate()
+    assert form.errors == {}
