3f4ca39883fa73ccd21b51ff03029534e2f78f32
Wiliam Souza <wiliamsouza83@gmail.com>
2014-02-19 16:11:02 -0300

    Added debug config

--- cars/__init__.py
+++ cars/__init__.py
@@ -4,6 +4,7 @@ from flask.ext.mongoengine import MongoEngine
 from flask.ext.login import LoginManager
 
 app = Flask(__name__)
+#app.config['DEBUG'] = True
 app.config['MONGODB_SETTINGS'] = {'DB': 'cars'}
 app.config['SECRET_KEY'] = 'super_secret_key'
 app.config['UPLOAD_FOLDER'] = '/srv/cars/cars/data/images'
